43b92d78ffdbe2aa043fa5258cad5a616fe93d28
Fahrettin Emre Dalga <fahrettinemredalga@Fahrettin-MacBook-Air.local>
2022-03-29 15:44:54 +0300

    firt

---
+++ string.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(strings.Contains("test", "es"))         //test içerisinde es var mı ?
+	fmt.Println(strings.Count("test", "t"))             // test içerisinde kaç tane  t var ?
+	fmt.Println(strings.HasPrefix("unit_test", "unit")) // unit kısmı alt çizgiden önce olduğu
+	// için prefix olarak adlandırılır. Prefix var mı diye ?
+	fmt.Println(strings.HasSuffix("test.rar", "rar")) // dosya uzantısı var mı ?
+	fmt.Println(strings.Index("test", "s"))           // s harfi test içinde kaçıncı index ?
+}
